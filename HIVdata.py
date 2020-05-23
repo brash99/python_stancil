875ec9ecb833961eb0fb3adf5b53d437b8324fa8
Edward Brash <brash@jlab.org>
2020-05-23 09:52:55 -0400

    Updates

--- HIVdata.py
+++ HIVdata.py
@@ -34,7 +34,7 @@ my_file.close()
 xdata = np.array(xdata)
 ydata = np.array(ydata)
 
-def fitfunction(x,a,b,c):
+def quadfunction(x,a,b,c):
     return a*x*x + b*x + c
 
 def expfunction(x,c,d):
@@ -47,7 +47,7 @@ plt.scatter(xdata,ydata,label='Experimental Data')
 plt.legend()
 
 
-popt,pcov = curve_fit(fitfunction,xdata,ydata)
+popt,pcov = curve_fit(quadfunction,xdata,ydata)
 
 popt2,pcov2 = curve_fit(expfunction,xdata,ydata)
 
@@ -56,21 +56,35 @@ print ("Fit1: ",popt,perr)
 perr2 = np.sqrt(np.diag(pcov2))
 print ("Fit2: ",popt2,perr2)
 
-#xdata = np.append(xdata,10.0)
-#xdata = np.append(xdata,12.0)
-#xdata = np.append(xdata,14.0)
-#xdata = np.append(xdata,16.0)
-#xdata = np.append(xdata,18.0)
+xmax = 12.5
+xfit = np.arange(0.0,xmax,0.1)
 
-print(xdata)
-
-plt.plot(xdata,fitfunction(xdata,*popt),'r-',
-         label='quad fit: a=%5.3f +/- %5.3f,\n     b=%5.3f +/- %5.3f,\n     c=%5.3f +/- %5.3f' % 
+plt.plot(xfit,quadfunction(xfit,*popt),'r-',
+         label='quad fit: a=%5.3f +/- %5.3f,\n b=%5.3f +/- %5.3f,\n c=%5.3f +/- %5.3f' % 
          (popt[0],perr[0],popt[1],perr[1],popt[2],perr[2]))
 
-plt.plot(xdata,expfunction(xdata,*popt2),'g-',
-         label='exp fit: a=%5.3f +/- %5.3f,\n     b=%5.3f +/- %5.3f' % 
-         (popt2[0],perr2[0],popt2[1],perr2[1]))
+xii = np.linspace(0.0,xmax,100)
+psi = np.random.multivariate_normal(popt,pcov,10000)
+ysamplei=np.asarray([quadfunction(xii,*pi) for pi in psi])
+loweri = np.percentile(ysamplei,16.0,axis=0)
+upperi = np.percentile(ysamplei,84.0,axis=0)
+plt.plot(xii,loweri,'r--')
+plt.plot(xii,upperi,'r--')
+
+#plt.plot(xfit,expfunction(xfit,*popt2),'g-',
+#         label='exp fit: a=%5.3f +/- %5.3f,\n b=%5.3f +/- %5.3f' % 
+#         (popt2[0],perr2[0],popt2[1],perr2[1]))
+#
+#xi = np.linspace(0.0,xmax,100)
+#ps = np.random.multivariate_normal(popt2,pcov2,10000)
+#ysample=np.asarray([expfunction(xi,*pi) for pi in ps])
+#lower = np.percentile(ysample,16.0,axis=0)
+#upper = np.percentile(ysample,84.0,axis=0)
+#plt.plot(xi,lower,'g--')
+#plt.plot(xi,upper,'g--')
+
+
+
 
 plt.legend()
 
---
+++ areacirc_improved.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 23 09:26:54 2020
+
+@author: brash
+"""
+
+import numpy as np
+
+try:
+    radius = float(input("Enter the radius of the circle: "))
+
+    circumference = 2.0*np.pi*radius
+    area = np.pi*radius**2
+
+    print("For a circle of radius %.2f, the circumference is %.2f and the area is %.2f" % (radius,area,circumference))
+    
+except ValueError:
+    print("The input data must be an integer or a float!!!")
+    
